9a597843897aff4372b755418a74677321dccd1d
kozhvat <kozhvat@mail.ru>
2024-03-08 21:39:31 +0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,174 @@
+'use strict';
+
+// console.log('-------------------------------граница задания')
+
+// const items = ['собака', 'кошка', 'попугай'];
+
+// getParrotIndex(items);
+
+// const parrotIndex = getParrotIndex(items);
+// console.log(parrotIndex);
+
+// function getParrotIndex(items) {
+//   let parrotIndex = null;
+//   items.forEach(function (item, index) {
+//     if (item === 'попугай') {
+//       parrotIndex = index;
+//     }
+//   });
+//   return parrotIndex;
+// }
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// multiple(2, 3);
+
+// function multiple(a, b) {
+//   const c = a * b;
+//   return c;
+// }
+// console.log('-------------------------------граница задания')
+
+// buildDate(12, 2, 2000);
+
+// function buildDate(day, month, year) {
+//   if (day < 10) {
+//     day = '0' + `${day}`;
+//   }
+//   if (month < 10) {
+//     month = '0' + `${month}`;
+//   }
+//   const stringDate = String(`${day}/${month}/${year}`);
+//   console.log(stringDate);
+//   return stringDate;
+// }
+
+// console.log('-------------------------------граница задания')
+
+// const age = 17;
+
+// isAdult(age);
+
+// function isAdult(age) {
+//   console.log(age >= 18);
+//   return age >= 18;
+// }
+
+// console.log('-------------------------------граница задания')
+
+// getMonthByNumber(2);
+
+// function getMonthByNumber(monthNumber) {
+//   switch (monthNumber) {
+//     case 1:
+//       console.log('Январь');
+//       return 'Январь';
+//     case 2:
+//       console.log('Февраль');
+//       return 'Февраль';
+//     case 3:
+//       console.log('Март');
+//       return 'Март';
+//     case 4:
+//       console.log('Апрель');
+//       return 'Апрель';
+//     case 5:
+//       console.log('Май');
+//       return 'Май';
+//     case 6:
+//       console.log('Июнь');
+//       return 'Июнь';
+//     case 7:
+//       console.log('Июль');
+//       return 'Июль';
+//     case 8:
+//       console.log('Август');
+//       return 'Август';
+//     case 9:
+//       console.log('Сентябрь');
+//       return 'Сентябрь';
+//     case 10:
+//       console.log('Октябрь');
+//       return 'Октябрь';
+//     case 11:
+//       console.log('Ноябрь');
+//       return 'Ноябрь';
+//     case 12:
+//       console.log('Декабрь');
+//       return 'Декабрь';
+//     default:
+//       console.log('месяца с таким номером нет');
+//       return 'месяца с таким номером нет';
+//   }
+// }
+
+// console.log('-------------------------------граница задания')
+
+// convertRub(500, 'USD');
+
+// function convertRub(valueRub, currency) {
+//   switch (currency) {
+//     case 'USD':
+//       valueRub /= 84.23;
+//       console.log(valueRub);
+//       return valueRub;
+//     case 'EUR':
+//       valueRub /= 92.07;
+//       console.log(valueRub);
+//       return valueRub;
+//     case 'KZT':
+//       valueRub /= 0.18838;
+//       console.log(valueRub);
+//       return valueRub;
+//     case 'AED':
+//       valueRub /= 22.93;
+//       console.log(valueRub);
+//       return valueRub;
+//     default:
+//       console.log('Такая валюта не поддерживается');
+//       return valueRub;
+//   }
+// }
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// printNumbers();
+
+// function printNumbers() {
+//   for (let i = -10; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+
+// console.log('-------------------------------граница задания')
+
+// printNumbersByRange(5, 15);
+
+// function printNumbersByRange(start, end) {
+//   for (let index = start; index <= end; index++) {
+//     console.log(index);
+//   }
+// }
+
+// console.log('-------------------------------граница задания')
+
+calculateNumbersSum([0, 1, 2, 4, 8])
+
+function calculateNumbersSum(numbers) {
+  for( let interimAmount = numbers[index]; interimAmount ) {  }
+}
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
+
+// console.log('-------------------------------граница задания')
